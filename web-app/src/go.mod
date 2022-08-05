d7b4de40cd251b11390e5d8483471c2bcdc07b84
Shutkin Yura <yshutkin@infra-lab.xyz>
2022-08-05 05:21:09 +0500

    new version of web-app
    * Go 1.18.2
    * logs in json
    * /ping
    * /net-check
    * /json

---
+++ web-app/src/go.mod
@@ -0,0 +1,3 @@
+module github.com/yura-shutkin/k8s-secrets-management/web-app
+
+go 1.18
--- web-app/src/main.go
+++ web-app/src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"html/template"
@@ -8,10 +9,62 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
-func ReadVarFromFile(filename string) (map[string]string, error) {
+func generateData() map[string]map[string]string {
+	vars := map[string]map[string]string{
+		"envs": make(map[string]string),
+	}
+
+	var envDirs string
+	envDirs = os.Getenv("SECRETS_DIRS")
+	if envDirs != "" {
+		var dirs []string
+		dirs = strings.Split(envDirs, ",")
+		fmt.Println(dirs)
+
+		var files []string
+
+		for idx := range dirs {
+			err := filepath.Walk(dirs[idx], func(path string, info os.FileInfo, err error) error {
+				files = append(files, path)
+				return nil
+			})
+			if err != nil {
+				fmt.Printf("An error occured: %s\n", err)
+			}
+		}
+
+		for idx := range files {
+			pair, err := readVarFromFile(files[idx])
+			if err != nil {
+				fmt.Printf("An error occured: %s\n", err)
+			}
+
+			if pair != nil {
+				_, ok := vars["file"]
+				if ok != true {
+					vars["file"] = map[string]string{
+						pair["key"]: pair["val"],
+					}
+				} else {
+					vars["file"][pair["key"]] = pair["val"]
+				}
+			}
+		}
+	}
+
+	for _, envs := range os.Environ() {
+		pair := strings.SplitN(envs, "=", 2)
+		vars["envs"][pair[0]] = pair[1]
+	}
+
+	return vars
+}
+
+func readVarFromFile(filename string) (map[string]string, error) {
 	info, err := os.Stat(filename)
 	if err != nil {
 		return nil, errors.New("something went wrong with access to file")
@@ -31,62 +84,147 @@ func ReadVarFromFile(filename string) (map[string]string, error) {
 	return pair, nil
 }
 
-func ShowVars(w http.ResponseWriter, r *http.Request) {
-	vars := map[string]map[string]string{
-		"envs": make(map[string]string),
-	}
-
-  var env_dirs string
-  env_dirs = os.Getenv("SECRETS_DIRS")
-	var dirs []string
-	dirs = strings.Split(env_dirs, ",")
-	fmt.Println(dirs)
-
-	var files []string
-
-	for idx := range dirs {
-		err := filepath.Walk(dirs[idx], func(path string, info os.FileInfo, err error) error {
-			files = append(files, path)
-			return nil
-		})
-		if err != nil {
-			fmt.Printf("An error occured: %s\n", err)
-		}
-	}
-
-	for _, envs := range os.Environ() {
-		pair := strings.SplitN(envs, "=", 2)
-		vars["envs"][pair[0]] = pair[1]
-	}
-
-	for idx := range files {
-		pair, err := ReadVarFromFile(files[idx])
-		if err != nil {
-			fmt.Printf("An error occured: %s\n", err)
-		}
-
-		if pair != nil {
-			_, ok := vars["file"]
-			if ok != true {
-				vars["file"] = map[string]string {
-					pair["key"]: pair["val"],
-				}
-			} else {
-				vars["file"][pair["key"]] = pair["val"]
-			}
-		}
-	}
+func renderWeb(w http.ResponseWriter, r *http.Request) {
+	// TODO: should return json
+	vars := generateData()
 
 	renderedPage, _ := template.ParseFiles("envs.gohtml")
 	err := renderedPage.Execute(w, vars)
 	if err != nil {
 		fmt.Printf("An error occured: %s\n", err)
 	}
-	fmt.Printf("{\"Method\": \"%s\", \"url\": \"%s\", \"Remote-Addr\":\"%s\"}\n", r.Method, r.URL, r.RemoteAddr)
+	// TODO: use a normal logger
+	logMessage := map[string]string{
+		"Method":      r.Method,
+		"URL":         r.URL.String(),
+		"Remote-Addr": r.RemoteAddr,
+	}
+	logMsgJSON, marshallErr := json.Marshal(logMessage)
+	if marshallErr != nil {
+		fmt.Printf("An error occured: %s\n", marshallErr)
+	}
+	fmt.Println(string(logMsgJSON))
+}
+
+func ping(w http.ResponseWriter, r *http.Request) {
+	data := map[string]string{
+		"status": "ok",
+	}
+
+	jsonData, marshallErr := json.Marshal(data)
+	if marshallErr != nil {
+		fmt.Printf("An error occured: %s\n", marshallErr)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonData)
+	// TODO: use a normal logger
+	logMessage := map[string]string{
+		"Method":      r.Method,
+		"URL":         r.URL.String(),
+		"Remote-Addr": r.RemoteAddr,
+	}
+	logMsgJSON, marshallErr := json.Marshal(logMessage)
+	if marshallErr != nil {
+		fmt.Printf("An error occured: %s\n", marshallErr)
+	}
+	fmt.Println(string(logMsgJSON))
+}
+
+func jsonEnvs(w http.ResponseWriter, r *http.Request) {
+	data := generateData()
+
+	jsonData, marshallErr := json.Marshal(data)
+	if marshallErr != nil {
+		fmt.Printf("An error occured: %s\n", marshallErr)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonData)
+	// TODO: use a normal logger
+	logMessage := map[string]string{
+		"Method":      r.Method,
+		"URL":         r.URL.String(),
+		"Remote-Addr": r.RemoteAddr,
+	}
+	logMsgJSON, marshallErr := json.Marshal(logMessage)
+	if marshallErr != nil {
+		fmt.Printf("An error occured: %s\n", marshallErr)
+	}
+	fmt.Println(string(logMsgJSON))
+}
+
+func checkServices(w http.ResponseWriter, r *http.Request) {
+	type host struct {
+		Addr string
+		Code string
+	}
+
+	type data struct {
+		Hosts []host
+		Error string
+	}
+
+	var response data
+
+	hostsList := os.Getenv("HOSTS")
+	if hostsList == "" {
+		response.Error = "Variable HOSTS is empty, should be list"
+	} else {
+		hosts := strings.Split(hostsList, ";")
+
+		for _, hostAddr := range hosts {
+			code := "0"
+			resp, respErr := http.Get(hostAddr)
+			if respErr != nil {
+				fmt.Printf("An error occured: %s\n", respErr)
+			} else {
+				code = strconv.Itoa(resp.StatusCode)
+			}
+
+			response.Hosts = append(response.Hosts, host{hostAddr, code})
+
+			// TODO: use a normal logger
+			logMessage := map[string]string{
+				"Host": hostAddr,
+				"Code": code,
+			}
+			logMsgJSON, marshallErr := json.Marshal(logMessage)
+			if marshallErr != nil {
+				fmt.Printf("An error occured: %s\n", marshallErr)
+			}
+			fmt.Println(string(logMsgJSON))
+		}
+	}
+
+	jsonData, dataMarshallErr := json.Marshal(response)
+	if dataMarshallErr != nil {
+		fmt.Printf("An error occured: %s\n", dataMarshallErr)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonData)
+	// TODO: use a normal logger
+	logMessage := map[string]string{
+		"Method":      r.Method,
+		"URL":         r.URL.String(),
+		"Remote-Addr": r.RemoteAddr,
+	}
+	logMsgJSON, marshallErr := json.Marshal(logMessage)
+	if marshallErr != nil {
+		fmt.Printf("An error occured: %s\n", marshallErr)
+	}
+	fmt.Println(string(logMsgJSON))
 }
 
 func main() {
-	fmt.Println("Server is starting on: 0.0.0.0:8080")
-	http.HandleFunc("/", ShowVars)
-	_ = http.ListenAndServe(":8080", nil)
+	listenAddr := os.Getenv("LISTEN_ADDR")
+	if listenAddr == "" {
+		listenAddr = "0.0.0.0:8080"
+	}
+
+	fmt.Printf("Server is starting on: %v\n", listenAddr)
+	http.HandleFunc("/", renderWeb)
+	http.HandleFunc("/json", jsonEnvs)
+	http.HandleFunc("/ping", ping)
+	http.HandleFunc("/net-check", checkServices)
+	_ = http.ListenAndServe(listenAddr, nil)
 }
